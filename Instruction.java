5c0b3f0aedf8ede52bceb2d2d076d0e542d2b54a
Nachiket Chauhan <chauhann4@vcu.edu>
2014-12-05 07:12:03 -0500

    Added J, BEQZ and BNEZ

--- Instruction.java
+++ Instruction.java
@@ -1,7 +1,7 @@
 /**
- * If you need the command, it is under Instruction.command
- * The registers are in string  form, under Instruction.reg1, reg2, reg3.
- * The immediate, which can also be the byte offset for lw and sw, is Instruction.immediate.
+ * If you need the command, it is under Instruction.command The registers are in string form, under
+ * Instruction.reg1, reg2, reg3. The immediate, which can also be the byte offset for lw and sw, is
+ * Instruction.immediate.
  */
 
 import java.util.Scanner;
@@ -14,154 +14,150 @@ import enums.Stage;
  */
 class Instruction {
 
-    Stage currentStage;
+  Stage currentStage;
 
-    // The command and then whatever else is after the command.
-    String command, data;
+  // The command and then whatever else is after the command.
+  String command, data;
 
-    // Each of the registers. There are going to be at max three, but not
-    // all of them have to be initialized.
-    String reg1, reg2, reg3;
+  // Each of the registers. There are going to be at max three, but not
+  // all of them have to be initialized.
+  String reg1, reg2, reg3;
 
-    // The immediate
-    int immediate;
+  // The immediate
+  int immediate;
 
-    public Instruction(String instruction) {
-	Scanner scan = new Scanner(instruction);
+  public Instruction(String instruction) {
+    Scanner scan = new Scanner(instruction);
 
-	command = scan.next();
+    command = scan.next();
 
-	data = scan.nextLine().trim();
-	scan.close();
+    data = scan.nextLine().trim();
+    scan.close();
 
-	String[] params = data.split(",");
+    String[] params = data.split(",");
 
-	if (params.length > 0) {
-	    reg1 = parseReg(params[0]);
+    if (params.length > 0) {
+      reg1 = parseReg(params[0]);
 
-	    if (command.equals("J")) {
-		scan = new Scanner(params[0]);
-		immediate = scan.nextInt();
-		scan.close();
-	    }
-	}
-
-	if (params.length > 1) {
-	    reg2 = parseReg(params[1]);
-
-	    // Grabbing the immediate from LW, SW, MV, BEQZ, BNEZ
-	    if (command.equals("LW") || command.equals("SW")
-		    || command.equals("MV") || command.equals("BEQZ")
-		    || command.equals("BNEZ")) {
-		scan = new Scanner(params[1]);
-		immediate = scan.nextInt();
-		scan.close();
-	    }
-
-	}
-
-	if (params.length > 2) {
-	    reg2 = parseReg(params[2]);
-
-	    // Grabbing the immediate from ADDI
-	    if (command.equals("ADDI")) {
-		scan = new Scanner(params[2]);
-		immediate = scan.nextInt();
-		scan.close();
-	    }
-	}
+      if (command.equals("J")) {
+        scan = new Scanner(params[0]);
+        immediate = scan.nextInt();
+        scan.close();
+      }
     }
 
-    // public Instruction(String instruction) {
-    //
-    // Scanner scan;
-    //
-    // currentStage = Stage.IF;
-    //
-    // /*
-    // * Splits instruction into three parts. params[0] will contain the
-    // * current pointer, params[1] contains the instruction type, params[2]
-    // * contains the registers where the instruction is being done.
-    // *
-    // * Ex. ADD $S2,$S2,$S3 params[0] = ADD, params[1] = $S2,$S2,$S3
-    // */
-    // String[] params = instruction.split(" ");
-    //
-    // command = params[0];
-    //
-    // /*
-    // * Splits the registers
-    // *
-    // * Ex. 12 ADD $S2,$S2,$S3 registers[0] = $S2, registers[1] = $S2,
-    // * registers[2] = $S3
-    // */
-    // String[] registers = params[1].split(",");
-    // }
+    if (params.length > 1) {
+      reg2 = parseReg(params[1]);
 
-    @Override
-    public String toString() {
-	// // Create stringbuilder
-	// StringBuilder builder = new StringBuilder();
-	//
-	// // Use stringerbuilder to extract values from instructionStatus map
-	// builder.append(instructionStatus.get(InstructionKeys.PC.getKey()))
-	// .append(instructionStatus.get(InstructionKeys.COMMAND_TYPE
-	// .getKey()));
-	//
-	// String[] registers = (String[]) instructionStatus
-	// .get(InstructionKeys.REGISTERS.getKey());
-	//
-	// // Combine the registers again
-	// for (String value : registers) {
-	// // For each register add a , after so something like 1,2,3,
-	// builder.append(value).append(",");
-	// }
-	//
-	// // Remove the last , because it is unneeded
-	// builder.deleteCharAt(builder.lastIndexOf(","));
-	//
-	// // Finally return
-	// return builder.toString();
+      // Grabbing the immediate from LW, SW, MV, BEQZ, BNEZ
+      if (command.equals("LW") || command.equals("SW") || command.equals("MV")
+          || command.equals("BEQZ") || command.equals("BNEZ")) {
+        scan = new Scanner(params[1]);
+        immediate = scan.nextInt();
+        scan.close();
+      }
+
     }
 
-    public String returnStage() {
-	return currentStage.toString();
-    }
+    if (params.length > 2) {
+      reg2 = parseReg(params[2]);
 
-    public void changeStage(Stage _stage) {
-	currentStage = _stage;
+      // Grabbing the immediate from ADDI
+      if (command.equals("ADDI")) {
+        scan = new Scanner(params[2]);
+        immediate = scan.nextInt();
+        scan.close();
+      }
     }
+  }
 
-    public boolean isBranch() {
-	return (command.equals("J") || command.equals("BEQZ") || command
-		.equals("BNEZ"));
-    }
+  // public Instruction(String instruction) {
+  //
+  // Scanner scan;
+  //
+  // currentStage = Stage.IF;
+  //
+  // /*
+  // * Splits instruction into three parts. params[0] will contain the
+  // * current pointer, params[1] contains the instruction type, params[2]
+  // * contains the registers where the instruction is being done.
+  // *
+  // * Ex. ADD $S2,$S2,$S3 params[0] = ADD, params[1] = $S2,$S2,$S3
+  // */
+  // String[] params = instruction.split(" ");
+  //
+  // command = params[0];
+  //
+  // /*
+  // * Splits the registers
+  // *
+  // * Ex. 12 ADD $S2,$S2,$S3 registers[0] = $S2, registers[1] = $S2,
+  // * registers[2] = $S3
+  // */
+  // String[] registers = params[1].split(",");
+  // }
 
-    public boolean writes() {
-	return command.equals("SLT") || command.equals("ADD")
-		|| command.equals("ADDI") || command.equals("LW")
-		|| command.equals("MV");
-    }
+  // public String toString() {
+  // // Create stringbuilder
+  // StringBuilder builder = new StringBuilder();
+  //
+  // // Use stringerbuilder to extract values from instructionStatus map
+  // builder.append(instructionStatus.get(InstructionKeys.PC.getKey()))
+  // .append(instructionStatus.get(InstructionKeys.COMMAND_TYPE
+  // .getKey()));
+  //
+  // String[] registers = (String[]) instructionStatus
+  // .get(InstructionKeys.REGISTERS.getKey());
+  //
+  // // Combine the registers again
+  // for (String value : registers) {
+  // // For each register add a , after so something like 1,2,3,
+  // builder.append(value).append(",");
+  // }
+  //
+  // // Remove the last , because it is unneeded
+  // builder.deleteCharAt(builder.lastIndexOf(","));
+  //
+  // // Finally return
+  // return builder.toString();
+  // }
 
-    private String parseReg(String firstParam) {
-	if (firstParam.contains("$t0"))
-	    return "$t0";
-	if (firstParam.contains("$t1"))
-	    return "$t1";
-	if (firstParam.contains("$t2"))
-	    return "$t2";
-	if (firstParam.contains("$t3"))
-	    return "$t3";
-	if (firstParam.contains("$t4"))
-	    return "$t4";
-	if (firstParam.contains("$s0"))
-	    return "$s0";
-	if (firstParam.contains("$s1"))
-	    return "$s1";
-	if (firstParam.contains("$s2"))
-	    return "$s2";
-	if (firstParam.contains("$s3"))
-	    return "$s3";
-	return "$zero";
-    }
+  public String returnStage() {
+    return currentStage.toString();
+  }
+
+  public void changeStage(Stage _stage) {
+    currentStage = _stage;
+  }
+
+  public boolean isBranch() {
+    return (command.equals("J") || command.equals("BEQZ") || command.equals("BNEZ"));
+  }
+
+  public boolean writes() {
+    return command.equals("SLT") || command.equals("ADD") || command.equals("ADDI")
+        || command.equals("LW") || command.equals("MV");
+  }
+
+  private String parseReg(String firstParam) {
+    if (firstParam.contains("$t0"))
+      return "$t0";
+    if (firstParam.contains("$t1"))
+      return "$t1";
+    if (firstParam.contains("$t2"))
+      return "$t2";
+    if (firstParam.contains("$t3"))
+      return "$t3";
+    if (firstParam.contains("$t4"))
+      return "$t4";
+    if (firstParam.contains("$s0"))
+      return "$s0";
+    if (firstParam.contains("$s1"))
+      return "$s1";
+    if (firstParam.contains("$s2"))
+      return "$s2";
+    if (firstParam.contains("$s3"))
+      return "$s3";
+    return "$zero";
+  }
 }
--- Pipelining.java
+++ Pipelining.java
@@ -13,246 +13,267 @@ import enums.Stage;
  */
 public class Pipelining {
 
-    public static Pipelining pipline = new Pipelining();
+  public static Pipelining pipline = new Pipelining();
 
-    private HashMap<String, Register> registers = new HashMap<>();
-    private HashMap<Integer, Integer> memory = new HashMap<>();
+  private HashMap<String, Register> registers = new HashMap<>();
+  private HashMap<Integer, Integer> memory = new HashMap<>();
 
-    // The list of instructions
-    private ArrayList<Instruction> instructions = new ArrayList<Instruction>();
+  // The list of instructions
+  private ArrayList<Instruction> instructions = new ArrayList<Instruction>();
 
-    // Flags to show if a register is being used or not.
-    private boolean[] free = new boolean[10];
+  // Flags to show if a register is being used or not.
+  private boolean[] free = new boolean[10];
 
-    // The mode is only used for deciding the algorithm, so in theory it could
-    // be a boolean.
-    private int mode, pc = 0, counter = 0;
+  // The mode is only used for deciding the algorithm, so in theory it could
+  // be a boolean.
+  private int mode, pc = 0, counter = 0;
 
-    private Instruction IF, ID, EX, MEM, WB;
+  private Instruction IF, ID, EX, MEM, WB;
 
-    // I'm doing this so that I don't have to worry about what is static and
-    // what isn't, it really doesn't do anything beneficial
-    public static void main(String[] args) {
-	try {
-	    // We could make initizalize the constructor
-	    pipline.initizalize();
-	    pipline.run(args);
-	} catch (FileNotFoundException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
+  // I'm doing this so that I don't have to worry about what is static and
+  // what isn't, it really doesn't do anything beneficial
+  public static void main(String[] args) {
+    try {
+      // We could make initizalize the constructor
+      pipline.initizalize();
+      pipline.run(args);
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  // Initialize registers
+  public void initizalize() {
+    registers.put(Registers.ZERO.returnKey(), new Register(0));
+
+    registers.put(Registers.T0.returnKey(), new Register(0));
+    registers.put(Registers.T1.returnKey(), new Register(0));
+    registers.put(Registers.T2.returnKey(), new Register(0));
+    registers.put(Registers.T3.returnKey(), new Register(0));
+    registers.put(Registers.T4.returnKey(), new Register(0));
+
+    registers.put(Registers.S0.returnKey(), new Register(0));
+    registers.put(Registers.S1.returnKey(), new Register(0));
+    registers.put(Registers.S2.returnKey(), new Register(0));
+    registers.put(Registers.S3.returnKey(), new Register(0));
+  }
+
+  public void run(String[] args) throws FileNotFoundException {
+    File firstFile = new File(args[0]);
+    File secondFile = new File(args[1]);
+    mode = Integer.parseInt(args[2]);
+
+    registers.clear();
+
+    for (int i = 0; i < 10; i++)
+      free[i] = true;
+    Scanner scan = new Scanner(firstFile);
+
+    // Add each instruction to the array of instructions.
+    while (scan.hasNext()) {
+      instructions.add(new Instruction(scan.nextLine()));
     }
 
-    // Initialize registers
-    public void initizalize() {
-	registers.put(Registers.ZERO.returnKey(), new Register(0));
+    scan.close();
+    scan = new Scanner(secondFile);
 
-	registers.put(Registers.T0.returnKey(), new Register(0));
-	registers.put(Registers.T1.returnKey(), new Register(0));
-	registers.put(Registers.T2.returnKey(), new Register(0));
-	registers.put(Registers.T3.returnKey(), new Register(0));
-	registers.put(Registers.T4.returnKey(), new Register(0));
-
-	registers.put(Registers.S0.returnKey(), new Register(0));
-	registers.put(Registers.S1.returnKey(), new Register(0));
-	registers.put(Registers.S2.returnKey(), new Register(0));
-	registers.put(Registers.S3.returnKey(), new Register(0));
+    // Extract all the values from memory
+    while (scan.hasNext()) {
+      String[] memoryLine = scan.nextLine().split(" ");
+      memory.put(Integer.parseInt(memoryLine[0]), Integer.parseInt(memoryLine[1]));
     }
 
-    public void run(String[] args) throws FileNotFoundException {
-	File firstFile = new File(args[0]);
-	File secondFile = new File(args[1]);
-	mode = Integer.parseInt(args[2]);
+    scan.close();
 
-	registers.clear();
+    if (mode == 1)
+      runMode1();
+    else
+      runMode2();
 
-	for (int i = 0; i < 10; i++)
-	    free[i] = true;
-	Scanner scan = new Scanner(firstFile);
+  }
 
-	// Add each instruction to the array of instructions.
-	while (scan.hasNext()) {
-	    instructions.add(new Instruction(scan.nextLine()));
-	}
+  private void runMode1() {
+    boolean exit = false;
 
-	scan.close();
-	scan = new Scanner(secondFile);
+    while (!exit) {
+      if (WB != null) {
 
-	// Extract all the values from memory
-	while (scan.hasNext()) {
-	    String[] memoryLine = scan.nextLine().split(" ");
-	    memory.put(Integer.parseInt(memoryLine[0]),
-		    Integer.parseInt(memoryLine[1]));
-	}
+        if (WB.writes()) {
+          registers.get(WB.reg1).setStatus(false);
+        }
 
-	scan.close();
+        switch (WB.command) {
 
-	if (mode == 1)
-	    runMode1();
-	else
-	    runMode2();
+        }
 
+        if (EX.command.equals("EXIT"))
+          exit = false;
+
+      }
+
+      WB = MEM;
+
+      if (MEM != null) {
+        switch (MEM.command) {
+          case "LW":
+            registers.get(MEM.reg1).value =
+                memory.get(registers.get(MEM.reg2).value + MEM.immediate);
+            break;
+          case "MV":
+            registers.get(MEM.reg1).value = MEM.immediate;
+            break;
+          case "SW":
+            memory.replace(registers.get(MEM.reg2).value + MEM.immediate,
+                registers.get(MEM.reg1).value);
+            break;
+        }
+
+      }
+
+      MEM = EX;
+
+      if (EX != null) {
+        switch (EX.command) {
+          case "ADDI":
+            registers.get(EX.reg1).value = registers.get(EX.reg2).value + EX.immediate;
+            break;
+          case "ADD":
+            registers.get(EX.reg1).value =
+                registers.get(EX.reg2).value + registers.get(EX.reg3).value;
+            break;
+          case "SLT":
+            registers.get(EX.reg1).value =
+                registers.get(EX.reg2).value < registers.get(EX.reg3).value ? 1 : 0;
+            break;
+        }
+
+      }
+
+      // Decode stage
+      // "Assume that source registers are read in the second half of the
+      // decode stage
+      if (ID != null) {
+        if (!registers.get(ID.reg2).getStatus() && !registers.get(ID.reg3).getStatus()) {
+          if (ID.writes()) {
+            registers.get(ID.reg1).setStatus(true);
+          }
+
+          if (ID.command.equals("SW")) {
+            registers.get(ID.reg2).setStatus(true);
+          }
+
+          EX = ID;
+          ID = IF;
+        } else {
+          EX = null;
+        }
+
+      } else {
+        ID = IF;
+      }
+
+      if (IF != null) {
+        // Making the instruction counter higher than
+        // the limit, so no instructions are loaded.
+        if (IF.isBranch()) {
+          switch (IF.command) {
+
+            case "J":
+              pc = pc + 4 + 4 * IF.immediate;
+              IF = instructions.get(pc);
+              break;
+
+            case "BEQZ":
+              if (registers.get(IF.reg1).value == 0) {
+                pc = pc + 4 + 4 * IF.immediate;
+                IF = instructions.get(pc);
+              }
+              break;
+
+            case "BNEZ":
+              if (registers.get(IF.reg1).value != 0) {
+                pc = pc + 4 + 4 * IF.immediate;
+                IF = instructions.get(pc);
+              }
+              break;
+          }
+
+
+
+        }
+
+      }
+
+      if (EX != null) {
+        IF = instructions.get(pc / 4);
+      }
+
+      // This is for the prints. It uses a while loop because there can be
+      // more than one in a row, and the pc shouldn't be changed.
+      boolean printFlag = true;
+
+      while (printFlag) {
+        pc += 4;
+
+        switch (instructions.get(pc / 4).command) {
+          case "PRINTPC":
+            printpc();
+            break;
+          case "PRINTREG":
+            printreg(instructions.get(pc / 4));
+            break;
+          case "PRINTEX":
+            printex();
+            break;
+          default:
+            printFlag = false;
+        }
+      }
+
+      counter++;
+    }
+  }
+
+  private void runMode2() {
+    // TODO: Implement forwarding here
+  }
+
+  private void printpc() {
+    System.out.println("PC = " + pc);
+  }
+
+  // Gets the first register of the instruction.
+  private void printreg(Instruction PR) {
+    System.out.println(registers.get(PR.reg1));
+  }
+
+  private void printex() {
+    System.out.println("Clock cycle = " + counter);
+
+    String ifString = "IF: NOP", idString = "ID: NOP", exString = "EX: NOP", memString = "MEM: NOP", wbString =
+        "WB: NOP";
+
+    // All instructions start out in IF, so this will break. Maybe use the
+    // PC to check what's in IF? Is this always correct?
+    for (Instruction temp : instructions) {
+      if (temp.currentStage == Stage.IF)
+        ifString = temp.toString();
+      if (temp.currentStage == Stage.ID)
+        idString = temp.toString();
+      if (temp.currentStage == Stage.EX)
+        exString = temp.toString();
+      if (temp.currentStage == Stage.MEM)
+        memString = temp.toString();
+      if (temp.currentStage == Stage.WB)
+        wbString = temp.toString();
     }
 
-    private void runMode1() {
-	boolean exit = false;
+    System.out.println(ifString);
+    System.out.println(idString);
+    System.out.println(exString);
+    System.out.println(memString);
+    System.out.println(wbString);
 
-	while (!exit) {
-	    if (WB != null) {
-
-		if (WB.writes()) {
-		    registers.get(WB.reg1).setStatus(false);
-		}
-
-		switch (WB.command) {
-
-		}
-
-		if (EX.command.equals("EXIT"))
-		    exit = false;
-
-	    }
-
-	    WB = MEM;
-
-	    if (MEM != null) {
-		switch (MEM.command) {
-		case "LW":
-		    registers.get(MEM.reg1).value = memory.get(registers
-			    .get(MEM.reg2).value + MEM.immediate);
-		    break;
-		case "MV":
-		    registers.get(MEM.reg1).value = MEM.immediate;
-		    break;
-		case "SW":
-		    memory.replace(registers.get(MEM.reg2).value
-			    + MEM.immediate, registers.get(MEM.reg1).value);
-		    break;
-		}
-
-	    }
-
-	    MEM = EX;
-
-	    if (EX != null) {
-		switch (EX.command) {
-		case "ADDI":
-		    registers.get(EX.reg1).value = registers.get(EX.reg2).value
-			    + EX.immediate;
-		    break;
-		case "ADD":
-		    registers.get(EX.reg1).value = registers.get(EX.reg2).value
-			    + registers.get(EX.reg3).value;
-		    break;
-		case "SLT":
-		    registers.get(EX.reg1).value = registers.get(EX.reg2).value < registers
-			    .get(EX.reg3).value ? 1 : 0;
-		    break;
-		}
-
-	    }
-
-	    // Decode stage
-	    // "Assume that source registers are read in the second half of the
-	    // decode stage
-	    if (ID != null) {
-		if (!registers.get(ID.reg2).getStatus()
-			&& !registers.get(ID.reg3).getStatus()) {
-		    if (ID.writes()) {
-			registers.get(ID.reg1).setStatus(true);
-		    }
-
-		    if (ID.command.equals("SW")) {
-			registers.get(ID.reg2).setStatus(true);
-		    }
-
-		    EX = ID;
-		    ID = IF;
-		} else {
-		    EX = null;
-		}
-
-	    } else {
-		ID = IF;
-	    }
-
-	    if (IF != null) {
-		// Making the instruction counter higher than
-		// the limit, so no instructions are loaded.
-		if (IF.isBranch()) {
-		    pc = instructions.size() * 4;
-		}
-
-	    }
-
-	    if (EX != null) {
-		IF = instructions.get(pc / 4);
-	    }
-
-	    // This is for the prints. It uses a while loop because there can be
-	    // more than one in a row, and the pc shouldn't be changed.
-	    boolean printFlag = true;
-
-	    while (printFlag) {
-		pc += 4;
-
-		switch (instructions.get(pc / 4).command) {
-		case "PRINTPC":
-		    printpc();
-		    break;
-		case "PRINTREG":
-		    printreg(instructions.get(pc / 4));
-		    break;
-		case "PRINTEX":
-		    printex();
-		    break;
-		default:
-		    printFlag = false;
-		}
-	    }
-
-	    counter++;
-	}
-    }
-
-    private void runMode2() {
-	// TODO: Implement forwarding here
-    }
-
-    private void printpc() {
-	System.out.println("PC = " + pc);
-    }
-
-    // Gets the first register of the instruction.
-    private void printreg(Instruction PR) {
-	System.out.println(registers.get(PR.reg1));
-    }
-
-    private void printex() {
-	System.out.println("Clock cycle = " + counter);
-
-	String ifString = "IF: NOP", idString = "ID: NOP", exString = "EX: NOP", memString = "MEM: NOP", wbString = "WB: NOP";
-
-	// All instructions start out in IF, so this will break. Maybe use the
-	// PC to check what's in IF? Is this always correct?
-	for (Instruction temp : instructions) {
-	    if (temp.currentStage == Stage.IF)
-		ifString = temp.toString();
-	    if (temp.currentStage == Stage.ID)
-		idString = temp.toString();
-	    if (temp.currentStage == Stage.EX)
-		exString = temp.toString();
-	    if (temp.currentStage == Stage.MEM)
-		memString = temp.toString();
-	    if (temp.currentStage == Stage.WB)
-		wbString = temp.toString();
-	}
-
-	System.out.println(ifString);
-	System.out.println(idString);
-	System.out.println(exString);
-	System.out.println(memString);
-	System.out.println(wbString);
-
-    }
+  }
 }
